b448722eeb56ab4be4b00f5f9bf532a95fd13795
alcid <alcides_thiago@hotmail.com>
2023-03-28 11:10:53 -0300

    Implementation class EscolhendoOrdemTeste.java

---
+++ src/test/java/dio/innovation/junit5exemplos/EscolhendoOrdemTeste.java
@@ -0,0 +1,31 @@
+package dio.innovation.junit5exemplos;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EscolhendoOrdemTeste {
+
+    @Order(4)
+    @Test
+    void validaFluxoA(){
+        Assertions.assertTrue(true);
+    }
+
+    @Order(3)
+    @Test
+    void validaFluxoB(){
+        Assertions.assertTrue(true);
+    }
+
+    @Order(2)
+    @Test
+    void validaFluxoC(){
+        Assertions.assertTrue(true);
+    }
+
+    @Order(1)
+    @Test
+    void validaFluxoD(){
+        Assertions.assertTrue(true);
+    }
+}
